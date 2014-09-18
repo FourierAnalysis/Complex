880293fa9ca0a9e40c0e4b0619266cb5c0033fa0
Raúl Alegre <raul.alemar@gmail.com>
2014-09-18 14:57:17 +0200

    adds animation with several circles

--- src/CP.js
+++ src/CP.js
@@ -5,13 +5,13 @@ CP.space  = 0.3;
 
 CP.range  = SD.rangeMaker  ({xMin: -CP.size/4, xMax: CP.size, yMin: -CP.size/4, yMax: CP.size});
 CP.scene  = SD.sceneMaker  ({div: CP.div, range: CP.range});
-CP.yAxis  = SD.lineMaker   ({x1:0, x2:0, y1:-CP.size, y2:CP.size, style:'->', arrowSize: CP.size/10});
-CP.xAxis  = SD.lineMaker   ({y1:0, y2:0, x1:-CP.size, x2:CP.size, style:'->', arrowSize: CP.size/10});
+CP.yAxis  = SD.lineMaker   ({x1:0, x2:0, y1:CP.range.yMin, y2:CP.range.yMax, style:'--', width:"1px"});
+CP.xAxis  = SD.lineMaker   ({y1:0, y2:0, x1:CP.range.xMin, x2:CP.range.xMax, style:'--', width:"1px"});
 
-CP.point  = SD.circleMaker ({ x:2,  y:3, r:0.05, svgAttributes: {fill: 'white'}});
-CP.vector = SD.lineMaker   ({x1:0, y1:0, x2:CP.point.x, y2:CP.point.y, style: '-->', arrowSize: CP.size/15, color:'red'});
-CP.xProj  = SD.lineMaker   ({x1:CP.point.x, y1:CP.point.y, x2:CP.point.x, y2:0, style:'--', color:'blue'});
-CP.yProj  = SD.lineMaker   ({x1:CP.point.x, y1:CP.point.y, x2:0, y2:CP.point.y, style:'--', color:'blue'});
+CP.point  = SD.circleMaker ({ x:2,  y:3, r:0.05, svgAttributes: {fill: '#253439'}});
+CP.vector = SD.lineMaker   ({x1:0, y1:0, x2:CP.point.x, y2:CP.point.y, style: '->', arrowSize: CP.size/10, color:'red'});
+CP.xProj  = SD.lineMaker   ({x1:CP.point.x, y1:CP.point.y, x2:CP.point.x, y2:0, style:'--', color:'#1e88ab'});
+CP.yProj  = SD.lineMaker   ({x1:CP.point.x, y1:CP.point.y, x2:0, y2:CP.point.y, style:'--', color:'#1e88ab'});
 CP.re     = SD.lineMaker   ({x1:0, y1:-CP.space, x2:CP.point.x, y2:-CP.space, style:'<->', width:'1px', arrowSize: CP.size/20});
 CP.im     = SD.lineMaker   ({x1:-CP.space, y1:0, x2:-CP.space, y2:CP.point.y, style:'<->', width:'1px', arrowSize: CP.size/20});
 
